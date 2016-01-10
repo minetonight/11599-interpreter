ce318d846cb145468fd5049b90391741c671e90b
Aleks <a.pavlov@robopartans.com>
2016-01-10 17:23:05 +0200

    refactored to OOP

--- numerology.py
+++ numerology.py
@@ -1,68 +1,73 @@
-birthday = input("Enter full birthday e.g. ddmmyyyy") 
-print(birthday+1) 
-birthdayStr = str(birthday)
+class CardCalculator:
+    
+    def __init__(self, birthday):
+        self.birthday = birthday
+        self.personalCard = [0] * 10
+        self.personalDate = int(str(birthday)[0:2]) 
+        self.personalNumber = 0
+        self.personalNumberSimplified = 0
+        
+        self.addToCard(birthday, True)
+        self.adjustPersonalNumber(self.personalNumber) 
+        self.personalNumberSimplified = self.simplifyNumber(self.personalNumber)
+        
+        print(self.personalCard) 
+        print(self.personalDate) 
+        print(self.personalNumber) 
+        print(self.personalNumberSimplified) 
+        
+    
+    def addToCard(self, number, addToPersonal=False):
+        strNum = str(number)
+        
+        for num in strNum:
+            num = int(num)
+            self.personalCard[num] += 1
+        
+        if addToPersonal:
+            self.personalNumber += num
+            
+    def simplifyNumber(self, number):
+        while number > 12:
+            if number > 12:
+                pNumStr = str(number) 
+                number = int(pNumStr[0]) + 	int(pNumStr[1])
+                self.addToCard(number)
+        
+        print(number)
+        return number
+
+    def adjustPersonalNumber(self, num):
+        year = int(str(self.birthday)[4:8]) 
+        print(year)
+        
+        if year < 2000:
+            self.addToCard(2)
+            result = self.personalNumber - 2
+            self.addToCard(result) 
+            print (result) 
+            self.simplifyNumber(result) 
+        else:
+            self.addToCard(19)
+            result = self.personalNumber + 19
+            self.addToCard(result) 
+            print(result) 
+            self.simplifyNumber(result) 
+        
+#eof class
+
+
+birthday = 21111985 #input("Enter full birthday e.g. ddmmyyyy") 
+print(birthday) 
 
 baseCard = [2,2,1,
 1,1,1,
 1,1,2] 
-
-
-personalCard = [0] * 9
-personalZeros = 0
-personalDate = int(birthdayStr[0:2]) 
-personalNumber = 0
-personalNumberSimplified = 0
-
-for num in birthdayStr:
-    num = int(num)
-    personalNumber += num
-    
-    if num == 0:
-        personalZeros += 1
-    else:
-        personalCard[num-1] += 1
-
-def addToCard(num):
-    print('todo') 
-    pass
-        
-addToCard(personalNumber) 
-
-def simplifyNumber(num):
-    print('todo') 
-    pass
-
-# def simplifyNumber
-personalNumberSimplified = personalNumber
-while personalNumberSimplified > 12:
-    if personalNumberSimplified > 12:
-        pNumStr = str(personalNumberSimplified) 
-        personalNumberSimplified = int(pNumStr[0]) + 	int(pNumStr[1])
-        addToCard(personalNumberSimplified)
-
-def adjustPersonalNumber(num):
-    year = int(birthdayStr[4:8]) 
-    print(year)
-    
-    if year < 2000:
-        addToCard(2)
-        result = personalNumber - 2
-        addToCard(result) 
-        print (result) 
-        simplifyNumber (result) 
-    else:
-        addToCard(19)
-        result = personalNumber + 19
-        addToCard(result) 
-        print(result) 
-        simplifyNumber (result) 
-    
-adjustPersonalNumber(personalNumber) 
-
 print(baseCard) 
-print(personalCard) 
-print(personalDate) 
-print(personalNumber) 
-print(personalNumberSimplified) 
+
+calculator = CardCalculator(birthday)
+
+
+
 
 
