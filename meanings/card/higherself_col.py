372947e746ea5b9aea30bd27332b66ddffff772f
Aleks <Aleksandarnp@gmail.com>
2016-03-05 09:52:12 +0200

    added calculation and basic interpretation for the columns

--- meanings/card/higherself_col.py
+++ meanings/card/higherself_col.py
@@ -1,3 +1,17 @@
 #-*-coding:utf8;-*-
 
-buf = " още няма данни, попълни card//storage/emulated/0/Android/data/me.sheimi.sgit/files/repo/11599-interpreter/meanings/card/higherself_col.py" 
+buf = "" 
+
+a = card[7] 
+b = card[8]
+c = card[9]
+norm = 4
+
+
+if a > 0 and b > 0 and c > 1:
+    buf = "Колоната за вътрешен свят е запълнена" 
+    power = a + b + c
+    buf += "\nСилата ѝ е %d при норма от %d"%(power, norm) 
+   
+   
+    
--- meanings/card/self_col.py
+++ meanings/card/self_col.py
@@ -1,3 +1,17 @@
 #-*-coding:utf8;-*-
 
-buf = " още няма данни, попълни card//storage/emulated/0/Android/data/me.sheimi.sgit/files/repo/11599-interpreter/meanings/card/self_col.py" 
+buf = "" 
+
+a = card[1] 
+b = card[2]
+c = card[3]
+norm = 5
+
+
+if a > 1 and b > 1 and c > 0:
+    buf = "Колоната за самостоятелност е запълнена" 
+    power = a + b + c
+    buf += "\nСилата ѝ е %d при норма от %d"%(power, norm) 
+   
+   
+    
